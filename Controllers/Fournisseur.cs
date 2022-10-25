a284d4b57f426ed1ff64358c07877c5207f03f77
Demeure Romain <demeureromain4@gmail.com>
2022-10-25 21:43:35 +0200

    add fournisseur need relations

--- Controllers/Fournisseur.cs
+++ Controllers/Fournisseur.cs
@@ -1,105 +1,105 @@
-// using Microsoft.AspNetCore.Mvc;
-// using cubeBackend.Models;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Threading.Tasks;
-// using Microsoft.AspNetCore.Http;
-// using Microsoft.EntityFrameworkCore;
-// namespace cubeBackend.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using cubeBackend.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+namespace cubeBackend.Controllers;
 
-// [ApiController]
-// [Route("[controller]")]
-// public class ProviderController : ControllerBase
-// {
-//     private readonly ILogger<ProviderController> _logger;
-//     private readonly cubeSQLContext _context;
+[ApiController]
+[Route("[controller]")]
+public class FournisseurController : ControllerBase
+{
+    private readonly ILogger<FournisseurController> _logger;
+    private readonly cubeSQLContext _context;
 
-//     public ProviderController(ILogger<ProviderController> logger, cubeSQLContext context)
-//     {
-//         _logger = logger;
-//         _context = context;
-//     }
+    public FournisseurController(ILogger<FournisseurController> logger, cubeSQLContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
 
-//     // GET ALL USERS
-//     [HttpGet]
-//     public async Task<ActionResult<IEnumerable<Fournisseur>>> GetFournisseurs()
-//     {
-//         return await _context.Fournisseurs.ToListAsync();
-//     }
+    // GET ALL FOURNISSEURS
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Fournisseur>>> GetFournisseurs()
+    {
+        return await _context.Fournisseurs.ToListAsync();
+    }
 
-//     // GET USER BY ID
-//     [HttpGet("{id}")]
-//     public async Task<ActionResult<Utilisateur>> GetFournisseur(int id)
-//     {
-//         var GetFournisseur = await _context.Fournisseurs.FindAsync(id);
-//         if (GetFournisseur == null)
-//         {
-//             return NotFound();
-//         }
+    // GET FOURNISSEUR BY ID
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Fournisseur>> GetFournisseur(int id)
+    {
+        var GetFournisseur = await _context.Fournisseurs.FindAsync(id);
+        if (GetFournisseur == null)
+        {
+            return NotFound();
+        }
 
-//         return GetFournisseur;
-//     }
+        return GetFournisseur;
+    }
 
-//     // CREATE USER
-//     [HttpPost]
-//     public async Task<ActionResult<Utilisateur>> PostUser(Utilisateur user)
-//     {
-//         _context.Utilisateurs.Add(user);
-//         await _context.SaveChangesAsync();
+    // CREATE FOURNISSEUR
+    [HttpPost]
+    public async Task<ActionResult<Fournisseur>> PostFournisseur(Fournisseur fournisseur)
+    {
+        _context.Fournisseurs.Add(fournisseur);
+        await _context.SaveChangesAsync();
 
-//         //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
-//         return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
-//     }
+        //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+        return CreatedAtAction(nameof(GetFournisseur), new { id = fournisseur.Id }, fournisseur);
+    }
 
-//     // UPDATE USER BY ID
-//     [HttpPut("{id}")]
-//     public async Task<IActionResult> PutUser(int id, Utilisateur user)
-//     {
-//         if (id != user.Id)
-//         {
-//             return BadRequest();
-//         }
+    // UPDATE FOURNISSEUR BY ID
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutFournisseur(int id, Fournisseur fournisseur)
+    {
+        if (id != fournisseur.Id)
+        {
+            return BadRequest();
+        }
 
-//         _context.Entry(user).State = EntityState.Modified;
+        _context.Entry(fournisseur).State = EntityState.Modified;
 
-//         try
-//         {
-//             await _context.SaveChangesAsync();
-//         }
-//         catch (DbUpdateConcurrencyException)
-//         {
-//             if (!UserExist(id))
-//             {
-//                 return NotFound();
-//             }
-//             else
-//             {
-//                 throw;
-//             }
-//         }
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!FournisseurExist(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
 
-//         return NoContent();
-//     }
+        return NoContent();
+    }
 
-//     // DELETE USER BY ID
-//     [HttpDelete("{id}")]
-//     public async Task<IActionResult> DeleteUser(int id)
-//     {
-//         var deleteUser = await _context.Utilisateurs.FindAsync(id);
-//         if (deleteUser == null)
-//         {
-//             return NotFound();
-//         }
+    // DELETE FOURNISSEUR BY ID
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteFournisseur(int id)
+    {
+        var DeleteFournisseur = await _context.Fournisseurs.FindAsync(id);
+        if (DeleteFournisseur == null)
+        {
+            return NotFound();
+        }
 
-//         _context.Utilisateurs.Remove(deleteUser);
-//         await _context.SaveChangesAsync();
+        _context.Fournisseurs.Remove(DeleteFournisseur);
+        await _context.SaveChangesAsync();
 
-//         return NoContent();
-//     }
+        return NoContent();
+    }
 
-//     // CHECK IF USER EXIST
-//     private bool UserExist(int id)
-//     {
-//         return _context.Utilisateurs.Any(e => e.Id == id);
-//     }
-// }
+    // CHECK IF FOURNISSEUR EXIST
+    private bool FournisseurExist(int id)
+    {
+        return _context.Fournisseurs.Any(e => e.Id == id);
+    }
+}
